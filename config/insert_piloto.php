627fe953e08ce6877d005523b9638ea4b27c369d
Eduard <eduardbadoyan@gmail.com>
2022-09-12 18:52:34 +0200

    Control de piloto existente

--- config/insert_piloto.php
+++ config/insert_piloto.php
@@ -1,34 +1,34 @@
 <?php
     
-    require_once "config.php";
+    // require_once "config.php";
 
 
-    $sql = "INSERT INTO pilotos (NombrePiloto,FechaNaciPiloto,EdadPiloto,LugarNaciPiloto,NacionalidadPiloto,InstaPiloto,TwitterPiloto,FotoPiloto) VALUES (?,?,?,?,?,?,?,?)";
+    // $sql = "INSERT INTO pilotos (NombrePiloto,FechaNaciPiloto,EdadPiloto,LugarNaciPiloto,NacionalidadPiloto,InstaPiloto,TwitterPiloto,FotoPiloto) VALUES (?,?,?,?,?,?,?,?)";
 
-    if ($stmt = mysqli_prepare($conexion, $sql)) {
+    // if ($stmt = mysqli_prepare($conexion, $sql)) {
         
-        mysqli_stmt_bind_param($stmt, "ssisssss", $NombrePiloto, $FechaNaciPiloto, $EdadPiloto, $LugarNaciPiloto, $NacionalidadPiloto, $InstaPiloto, $TwitterPiloto, $FotoPiloto);
+    //     mysqli_stmt_bind_param($stmt, "ssisssss", $NombrePiloto, $FechaNaciPiloto, $EdadPiloto, $LugarNaciPiloto, $NacionalidadPiloto, $InstaPiloto, $TwitterPiloto, $FotoPiloto);
 
-        $NombrePiloto =         $_REQUEST['NombrePiloto'];
-        $FechaNaciPiloto =      $_REQUEST['FechaNaci'];
-        $EdadPiloto =           $_REQUEST['Edad'];
-        $LugarNaciPiloto =      $_REQUEST['LugarNaci'];
-        $NacionalidadPiloto =   $_REQUEST['Nacionalidad'];
-        $InstaPiloto =          $_REQUEST['Instagram'];
-        $TwitterPiloto =        $_REQUEST['Twitter'];
-        $FotoPiloto =           $_REQUEST['FotoPiloto'];
+    //     $NombrePiloto =         $_REQUEST['NombrePiloto'];
+    //     $FechaNaciPiloto =      $_REQUEST['FechaNaci'];
+    //     $EdadPiloto =           $_REQUEST['Edad'];
+    //     $LugarNaciPiloto =      $_REQUEST['LugarNaci'];
+    //     $NacionalidadPiloto =   $_REQUEST['Nacionalidad'];
+    //     $InstaPiloto =          $_REQUEST['Instagram'];
+    //     $TwitterPiloto =        $_REQUEST['Twitter'];
+    //     $FotoPiloto =           $_REQUEST['FotoPiloto'];
 
-        if(mysqli_stmt_execute($stmt)){
-            echo "Se ha añadido el nuevo piloto.";
-        } else {
-            echo "Error: no se ha podido ejecutar la consulta: $sql. " . mysqli_error($conexion);
-        }
-    } else {
-        echo "Error: no se ha podido ejecutar la consulta: $sql. " . mysqli_error($conexion);
-    }
+    //     if(mysqli_stmt_execute($stmt)){
+    //         echo "Se ha añadido el nuevo piloto.";
+    //     } else {
+    //         echo "Error: no se ha podido ejecutar la consulta: $sql. " . mysqli_error($conexion);
+    //     }
+    // } else {
+    //     echo "Error: no se ha podido ejecutar la consulta: $sql. " . mysqli_error($conexion);
+    // }
 
-    mysqli_stmt_close($stmt);
+    // mysqli_stmt_close($stmt);
 
-    mysqli_close($conexion);
+    // mysqli_close($conexion);
 
 
--- form_insert_piloto.php
+++ form_insert_piloto.php
@@ -10,6 +10,76 @@
 
     control_cabecera_usuarios_logueados($titulo);
 
+
+/////////////////////////////test//////////////////////////////////
+$NombrePiloto = $NombrePiloto_err = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (empty(trim($_POST['NombrePiloto']))) {
+        $NombrePiloto_err = "Ingrese nombre y apellido del piloto";
+    } //elseif (!preg_match('/^[a-zA-Z]+$/', trim($_POST["NombrePiloto"]))){
+       // $NombrePiloto_err = "Nombre de piloto solo puede contener letras";
+    //} 
+    else {
+
+        $consulta = "SELECT IdPiloto FROM `pilotos` WHERE NombrePiloto = ?";
+
+        if ($stmt = mysqli_prepare($conexion, $consulta)) {
+            mysqli_stmt_bind_param($stmt, "s" , $param_NombrePiloto);
+
+            $param_NombrePiloto = trim($_POST['NombrePiloto']);
+
+            if (mysqli_stmt_execute($stmt)) {
+
+                mysqli_stmt_store_result($stmt);
+
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    $NombrePiloto_err = "Este Piloto ya esta registrado en la Base de Datos";
+                } else {
+                    $NombrePiloto = trim($_POST['NombrePiloto']);
+                }
+            } else {
+                echo "¡Anda! Ha pasado algo raro. Intentalo de nuevo.";
+            }
+        }
+        mysqli_stmt_close($stmt);
+
+    }
+
+    if (empty($NombrePiloto_err)) {
+
+        $sql = "INSERT INTO pilotos (NombrePiloto,FechaNaciPiloto,EdadPiloto,LugarNaciPiloto,NacionalidadPiloto,InstaPiloto,TwitterPiloto,FotoPiloto) VALUES (?,?,?,?,?,?,?,?)";
+
+        if ($stmt = mysqli_prepare($conexion, $sql)) {
+            
+            mysqli_stmt_bind_param($stmt, "ssisssss", $NombrePiloto, $FechaNaciPiloto, $EdadPiloto, $LugarNaciPiloto, $NacionalidadPiloto, $InstaPiloto, $TwitterPiloto, $FotoPiloto);
+    
+            $NombrePiloto =         $_REQUEST['NombrePiloto'];
+            $FechaNaciPiloto =      $_REQUEST['FechaNaci'];
+            $EdadPiloto =           $_REQUEST['Edad'];
+            $LugarNaciPiloto =      $_REQUEST['LugarNaci'];
+            $NacionalidadPiloto =   $_REQUEST['Nacionalidad'];
+            $InstaPiloto =          $_REQUEST['Instagram'];
+            $TwitterPiloto =        $_REQUEST['Twitter'];
+            $FotoPiloto =           $_REQUEST['FotoPiloto'];
+    
+            if(mysqli_stmt_execute($stmt)){
+                echo "Se ha añadido el nuevo piloto.";
+            } else {
+                echo "Error: no se ha podido ejecutar la consulta: $sql. " . mysqli_error($conexion);
+            }
+        } else {
+            echo "Error: no se ha podido ejecutar la consulta: $sql. " . mysqli_error($conexion);
+        }
+    
+        mysqli_stmt_close($stmt);
+    
+        mysqli_close($conexion);
+    }
+
+}
+/////////////////////////////test//////////////////////////////////
 ?>
 
 <div class="container mt-5">
@@ -41,10 +111,11 @@
             <h2>Añadir nuevo piloto</h2>
             <p>Para añadir nuevo piloto debe rellenar el siguiente formulario con los datos del Piloto.</p>
             <br>
-            <form action="config/insert_piloto.php" method="post">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                 <p>
                     <!--<label for="NombrePiloto">Nombre del Piloto:</label>-->
-                    <input type="text" class="form-control mt-3" name="NombrePiloto" placeholder="Nombre Piloto">
+                    <input type="text" class="form-control mt-3 <?php echo (!empty($NombrePiloto_err)) ? 'is-invalid' : ''; ?>" name="NombrePiloto" placeholder="Nombre Piloto (Nombre Apellido)" value="<?php echo $NombrePiloto; ?>">
+                    <span class="invalid-feedback"><?php echo $NombrePiloto_err; ?></span>
                 </p>
                 <p>
                     <!--<label for="FechaNaci">Fecha nacimiento:</label>-->
@@ -73,7 +144,10 @@
                 <p>
                     <input type="text" class="form-control mt-3" name="FotoPiloto">
                 </p>
-                <button type="submit" class="btn btn-primary">Enviar</button>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Enviar">
+                    <input type="reset" class="btn btn-secondary ml-2" value="Borrar">
+                </div>
             </form>
         </div>
     </div>
