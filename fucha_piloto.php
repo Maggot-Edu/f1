6a0d67c6c44c090c3cfc96ab0c08733500a7a37a
Eduard <eduardbadoyan@gmail.com>
2022-09-15 18:36:05 +0200

    Creacion ficha piloto envio de datos URL

---
+++ fucha_piloto.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once "config/config.php";
+    session_start();
+
+    $titulo = $_GET["nombre"];
+    require_once "includes/cabecera.php";
+    // Comprobamos si esta logueado o no
+    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+        if ($_SESSION["perfil"] == "admin") {
+            require_once "includes/menu_admin.php";
+        } else {
+            require_once "includes/menu_user.php";
+        }
+    } else {
+        require_once "includes/menu.php";
+    }
+?>
+
+<h1>
+    <?php echo $_GET["piloto"]; ?>
+</h1>
+<?php
+    require_once "includes/footer.php";
+?>
